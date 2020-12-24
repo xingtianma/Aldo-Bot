fc272ef60a424e9384d8b6e1645108b182a7b338
darmthealarm <kma091504@gmail.com>
2020-12-24 03:07:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ client.on('message', message =>{
 
         for(x = 0; x < str.length; x++)
         {
-            y = str.charAt(x)
-            message.channel.send(toUpperCase(y))
+            testStr = str.charAt(x)
+            message.channel.send(testStr.toUpperCase())
             /*
             let number = Math.floor(Math.random() * 3) 
             if(number < 1)
