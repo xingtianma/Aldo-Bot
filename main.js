a0f34e2818a7506a4f76b0743f3d43150838ddf7
darmthealarm <kma091504@gmail.com>
2020-12-24 03:00:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ client.on('message', message =>{
         for(x = 0; x < str.length; x++)
         {
             y = str.charAt(x)
-            message.channel.send(x)
+            message.channel.send(y)
             /*
             let number = Math.floor(Math.random() * 3) 
             if(number < 1)
