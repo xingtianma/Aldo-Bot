7a6735ee024ece5a84aa4b5629e020dd260f98db
darmthealarm <kma091504@gmail.com>
2021-01-05 13:24:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -120,7 +120,7 @@ client.on('message', message =>{
     const command = args.shift().toLowerCase();
 
     if(command === 'list'){
-        message.channel.send('```Dab Memes\n!debby\n!sarah\n!nein\n!john\n!kevin\n!alex\n!sanskevin\n!sansalex\n!avatar\n!beauty\n!beauty2\n!amongus\n!suli\n!jordan\n!jake\n!cheese\n!cockandmen\n!waterunicorn\n!chikadance\n!angel\n!noracism\n!joash\n!spooky\n//////////////////////\nKevin Torture\nspam\nmurder\n!mute **in progress**```');
+        message.channel.send('```Dab Memes\n!debby\n!sarah\n!nein\n!john\n!kevin\n!alex\n!sanskevin\n!sansalex\n!avatar\n!beauty\n!beauty2\n!amongus\n!suli\n!jordan\n!jake\n!cheese\n!cockandmen\n!waterunicorn\n!chikadance\n!angel\n!noracism\n!joash\n!spooky\n!snake\n//////////////////////\nKevin Torture\nspam\nmurder\n!mute **in progress**```');
     }
     else if(command === 'debby'){
         message.channel.send('is now a very big simp');
