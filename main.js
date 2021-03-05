dc76d06a3d6a06ab8acc7a14b38864872b10f840
darmthealarm <kma091504@gmail.com>
2021-03-05 01:51:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -54,6 +54,16 @@ client.on('message', message =>{
         message.channel.send('https://cdn.discordapp.com/attachments/688142646207578208/756733951715836014/image0.png')
     if(message.content.toLowerCase() === 'yo whos sus?')
         message.channel.send('idk man <@434877530915471372> kinda sus ngl bro')
+    if(message.content.toLowerCase() === 'wakeupsuejin')
+    {
+        x = 0;
+        y = 20;
+        while(x < y)
+        {
+            message.channel.send('<@639204215113252914>')
+            x = x + 1;
+        }
+    }
     if(message.content.toLowerCase() === 'spam')
     {
         if(message.author.id == '282698201289588736')
